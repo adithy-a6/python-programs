bf7c11ee572c63a43c34a364b17b1b90f1b7720a
adithy-a6 <chn25mca022@ceconline.edu>
2025-10-28 14:09:10 +0530

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,26 @@
+from graphics.rectangle import area as rect_area, perimeter as rect_perimeter
+from graphics.circle import area as circle_area, perimeter as circle_perimeter
+from graphics._3D_graphics.cuboid import surface_area as cuboid_surface_area,volume as cuboid_volume
+from graphics._3D_graphics.sphere import surface_area as sphere_surface_area,volume as sphere_volume
+print("Enter the length and breadth of rectangle:")
+l=int(input("Length:"))
+b=int(input("Breadth:"))
+print("Rectangle Area:",rect_area(l,b))
+print("Rectangle Perimeter:",rect_perimeter(l,b))
+print("\n\n")
+print("Enter the radius of circle")
+radius=int(input("Radius"))
+print ("Circle area:",circle_area(radius))
+print ("Circle Perimeter:",circle_perimeter(radius))
+print("\n\n")
+print("Enter the dimension of cuboid:")
+length=float(input("Length:"))
+width=float(input("width:"))
+height=float(input("height:"))
+print("Cuboid surface area:",cuboid_surface_area(length,width,height))
+print("Cuboid volume:",cuboid_volume(length,width,height))
+print("\n\n")
+print("Enter the radius of sphere")
+radius=float(input("Radius"))
+print("Sphere surface Area",sphere_surface_area(radius))
+print("Spheree volume",sphere_volume(radius))
